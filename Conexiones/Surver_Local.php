6794bff3469da89de2b3de753873ad461775dc81
victor-rivera24 <vvrh.victor.rivera@gmail.com>
2022-10-31 16:30:57 -0600

    Cambios Generales v2

--- Conexiones/Surver_Local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('SERVIDOR_Surver','172.16.103.113');
-define('BASEDATOS_Surver','Surver-18-02');
-define('USUARIO_Surver','adempiere');
-define('CONTRASENIA_Surver','[BDEncuestas.!/]');
-define('PUERTO_Surver','65432');
-define('ESQUEMA_Surver','refividrio');
-?>
