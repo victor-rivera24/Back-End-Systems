abd1c531494f776b0280b3250d51c449aa97f006
victor-rivera24 <vvrh.victor.rivera@gmail.com>
2022-10-31 16:33:13 -0600

    Cambios Generales v3

--- Conexiones/ADempiere_RFV_Remoto.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('SERVIDOR_RFV','192.168.193.25');
-define('BASEDATOS_RFV','RFV221218');
-define('USUARIO_RFV','adempiere');
-define('CONTRASENIA_RFV','[rfhood_.?/]');
-define('PUERTO_RFV','65432');
-define('ESQUEMA_RFV','adempiere');
-?>
--- Conexiones/Cerberus_Remoto.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('SERVIDOR_Cerberus','192.168.193.157');
-define('BASEDATOS_Cerberus','Cerberus');
-define('USUARIO_Cerberus','sa');
-define('CONTRASENIA_Cerberus','Compac12');
-define('PUERTO_Cerberus','1433');
-?>
